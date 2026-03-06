e067f195be3403a24bc776131f277c36b3221e31
rohanbuyyar15 <ssrohanbuyyar@gmail.com>
2026-03-06 12:29:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ export default function App() {
     formData.append('image', file);
 
     try {
-      const response = await fetch('http://localhost:5000/api/predict', {
+      const response = await fetch('https://gomata-ai.onrender.com/api/predict', {
         method: 'POST',
         body: formData,
       });
